e1af740c07be4e8fba330fafc9b8c56a26a0c1d6
Jiufen <pardod479@gmail.com>
2020-10-07 13:06:19 -0500

    Build

---
+++ src/components/UnityWebGL.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function UnityWebGL() {
+    return (
+        <div id="unity-container" class="unity-desktop">
+        <canvas id="unity-canvas"></canvas>
+        <div id="unity-loading-bar">
+            <div id="unity-logo"></div>
+            <div id="unity-progress-bar-empty">
+            <div id="unity-progress-bar-full"></div>
+            </div>
+        </div>
+        <div id="unity-footer">
+            <div id="unity-webgl-logo"></div>
+            <div id="unity-fullscreen-button"></div>
+            <div id="unity-build-title">BoomAway</div>
+        </div>
+        </div>
+    )
+}
+
+export default UnityWebGL
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Pages.css';
-import UnityWebPlayer from '../components/UnityWebPlayer';
+import UnityWebGL from'../components/UnityWebGL';
 
 
 function Home() {
     return (
         <div className="home">
             <div className="unityWebPlayer">
-                <UnityWebPlayer/>
+                <UnityWebGL/>
             </div>
             <div className="contentCard">
                 <div className="container" >
@@ -21,7 +21,9 @@ function Home() {
                         </div>
                         <div class="col-xl-6 col-12">
                             <p className="paragraph">
-                            a
+                            Este es un juego realizado como proyecto de grado de Ingeniería de Sistemas y Computación en la Universidad de los Andes (Bogotá, Colombia),
+                            bajo la categoria de juego experimental. La parte experimental es un juego de plataformas 2D donde el protagonista no puede saltar, y el resultado es un 
+                            videojuego de puzzle en el cual el personaje tiene control de diferentes explosivos para concatenar acciones que lo lleven a alcanzar el final de los niveles.
                             </p>
                         </div>
                     </div>
